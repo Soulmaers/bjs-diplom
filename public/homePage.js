cdb840d6d5e9abc398c095e5609af061c4de1ecd
Soulmaers <certiss@ya.ru>
2022-03-16 08:23:30 +0300

    Первая часть с авторизацией

---
+++ public/homePage.js
@@ -0,0 +1,14 @@
+"use strick"
+/*const logoutButton = new LogoutButton();
+logoutButton.action=(logout)=>{
+    ApiConnector.logout(logout, callback=>{
+        if(callback.success===false){
+            userForm.setLoginErrorMessage(callback.error);
+        }else{
+            location.reload();
+        }
+
+    })
+
+
+}*/
