53e2586a65094372e0c6b45ee1262a1ae52cc3c6
Soulmaers <certiss@ya.ru>
2022-03-18 16:48:08 +0300

    ver 1.4 Готово

--- public/homePage.js
+++ public/homePage.js
@@ -19,13 +19,81 @@ ApiConnector.current(response => {
 //получение курсов текущих валют
 
 const ratesBoard = new RatesBoard();
-const func = () => {
-    ApiConnector.getStocks = (response => {
+const func = (ratesBoard) => {
+    ApiConnector.getStocks(response => {
         if (response.success) {
             ratesBoard.clearTable();
             ratesBoard.fillTable(response.data);
         };
     });
 }
-func();
+func(ratesBoard);
 setInterval(func, 60000);
+
+//операция с деньгами-пополнение баланса
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+    ApiConnector.addMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+        };
+        const message = response.error || 'Счет пополнен';
+        moneyManager.setMessage(response.success, message);
+    });
+}
+// операция с деньгами-конвертирование валюты
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+        };
+        const message = response.error || 'Конвертация выполнена';
+        moneyManager.setMessage(response.success, message);
+    });
+};
+// операция с деньгами-перевод валюты
+
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, response => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+        };
+        const message = response.error || 'Перевод выполнен';
+        moneyManager.setMessage(response.success, message);
+    });
+
+};
+
+//работа с избранным-начальный список избранного
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+    }
+});
+//работа с избранным-добавления пользователя в список избранных
+favoritesWidget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+        const message = response.error || 'Пользователь добавлен';
+        favoritesWidget.setMessage(response.success, message);
+    });
+};
+//работа с избранным-удаление пользователя из избранного
+favoritesWidget.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+        }
+        const message = response.error || 'Пользователь удалён';
+        moneyManager.setMessage(response.success, message);
+    });
+}
